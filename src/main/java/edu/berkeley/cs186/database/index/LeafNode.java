33579933e82aec15117962f8c03106307545bf04
anglenet <anglenet@sina.cn>
2021-12-18 21:27:53 +0800

    Add insert/get api for LeafNode

--- src/main/java/edu/berkeley/cs186/database/index/LeafNode.java
+++ src/main/java/edu/berkeley/cs186/database/index/LeafNode.java
@@ -19,11 +19,11 @@ import java.util.*;
  * persisted on a single page; see toBytes and fromBytes for details on how a
  * leaf is serialized. For example, here is an illustration of two order 2
  * leafs connected together:
- *
- *   leaf 1 (stored on some page)          leaf 2 (stored on some other page)
- *   +-------+-------+-------+-------+     +-------+-------+-------+-------+
- *   | k0:r0 | k1:r1 | k2:r2 |       | --> | k3:r3 | k4:r4 |       |       |
- *   +-------+-------+-------+-------+     +-------+-------+-------+-------+
+ * <p>
+ * leaf 1 (stored on some page)          leaf 2 (stored on some other page)
+ * +-------+-------+-------+-------+     +-------+-------+-------+-------+
+ * | k0:r0 | k1:r1 | k2:r2 |       | --> | k3:r3 | k4:r4 |       |       |
+ * +-------+-------+-------+-------+     +-------+-------+-------+-------+
  */
 class LeafNode extends BPlusNode {
     // Metadata about the B+ tree that this node belongs to.
@@ -106,6 +106,7 @@ class LeafNode extends BPlusNode {
     private Optional<Long> rightSibling;
 
     // Constructors ////////////////////////////////////////////////////////////
+
     /**
      * Construct a brand new leaf node. This constructor will fetch a new pinned
      * page from the provided BufferManager `bufferManager` and persist the node
@@ -114,8 +115,8 @@ class LeafNode extends BPlusNode {
     LeafNode(BPlusTreeMetadata metadata, BufferManager bufferManager, List<DataBox> keys,
              List<RecordId> rids, Optional<Long> rightSibling, LockContext treeContext) {
         this(metadata, bufferManager, bufferManager.fetchNewPage(treeContext, metadata.getPartNum()),
-             keys, rids,
-             rightSibling, treeContext);
+                keys, rids,
+                rightSibling, treeContext);
     }
 
     /**
@@ -147,30 +148,76 @@ class LeafNode extends BPlusNode {
     @Override
     public LeafNode get(DataBox key) {
         // TODO(proj2): implement
-
-        return null;
+        LeafNode cur = this;
+        while (cur != null) {
+            // cur -> always point to node to be examined
+            if (cur.keys.isEmpty()) {
+                break;
+            }
+            DataBox last = cur.keys.get(cur.keys.size() - 1);
+            if (key.compareTo(last) <= 0) {
+                // must be on this node
+                break;
+            }
+            // key > last
+            if (!cur.rightSibling.isPresent()) {
+                // no tail anymore, it must be in this node theoretically event
+                // it's not in it, actually.
+                break;
+            }
+            // must be on its right sibling
+            cur = fromBytes(cur.metadata, cur.bufferManager, cur.treeContext, cur.rightSibling.get());
+        }
+        return cur;
     }
 
     // See BPlusNode.getLeftmostLeaf.
     @Override
     public LeafNode getLeftmostLeaf() {
         // TODO(proj2): implement
+        // the left most node of a leaf node is itself.
 
-        return null;
+        return this;
     }
 
     // See BPlusNode.put.
     @Override
     public Optional<Pair<DataBox, Long>> put(DataBox key, RecordId rid) {
         // TODO(proj2): implement
-
-        return Optional.empty();
+        // there is still place -> add <key, rid> and resort
+        // no place -> split this node
+        if(this.getKey(key).isPresent()) {
+            throw new BPlusTreeException("Duplicate key");
+        }
+        this.keys.add(key);
+        this.keys.sort(DataBox::compareTo);
+        this.rids.add(rid);
+        this.rids.sort(RecordId::compareTo);
+        int order = this.metadata.getOrder();
+        Optional<Pair<DataBox, Long>> opt = Optional.empty();
+        if (this.keys.size() > 2 * order) {
+            // need to split
+            List<DataBox> rightKeys = this.keys.subList(order, this.keys.size());
+            List<RecordId> rightRids = this.rids.subList(order, this.rids.size());
+            ArrayList<DataBox> boxes = new ArrayList<>(2 * order);
+            boxes.addAll(rightKeys);
+            ArrayList<RecordId> recs = new ArrayList<>(2 * order);
+            recs.addAll(rightRids);
+            LeafNode sib = new LeafNode(this.metadata, this.bufferManager, boxes,
+                    recs, Optional.empty(), this.treeContext);
+            opt = Optional.of(new Pair<>(rightKeys.get(0), sib.page.getPageNum()));
+            rightKeys.clear();
+            rightRids.clear();
+            this.rightSibling = Optional.of(sib.page.getPageNum());
+        }
+        this.sync();
+        return opt;
     }
 
     // See BPlusNode.bulkLoad.
     @Override
     public Optional<Pair<DataBox, Long>> bulkLoad(Iterator<Pair<DataBox, RecordId>> data,
-            float fillFactor) {
+                                                  float fillFactor) {
         // TODO(proj2): implement
 
         return Optional.empty();
@@ -185,7 +232,10 @@ class LeafNode extends BPlusNode {
     }
 
     // Iterators ///////////////////////////////////////////////////////////////
-    /** Return the record id associated with `key`. */
+
+    /**
+     * Return the record id associated with `key`.
+     */
     Optional<RecordId> getKey(DataBox key) {
         int index = keys.indexOf(key);
         return index == -1 ? Optional.empty() : Optional.of(rids.get(index));
@@ -215,7 +265,9 @@ class LeafNode extends BPlusNode {
         return page;
     }
 
-    /** Returns the right sibling of this leaf, if it has one. */
+    /**
+     * Returns the right sibling of this leaf, if it has one.
+     */
     Optional<LeafNode> getRightSibling() {
         if (!rightSibling.isPresent()) {
             return Optional.empty();
@@ -225,7 +277,9 @@ class LeafNode extends BPlusNode {
         return Optional.of(LeafNode.fromBytes(metadata, bufferManager, treeContext, pageNum));
     }
 
-    /** Serializes this leaf to its page. */
+    /**
+     * Serializes this leaf to its page.
+     */
     private void sync() {
         page.pin();
         try {
@@ -306,8 +360,8 @@ class LeafNode extends BPlusNode {
     /**
      * Given a leaf with page number 1 and three (key, rid) pairs (0, (0, 0)),
      * (1, (1, 1)), and (2, (2, 2)), the corresponding dot fragment is:
-     *
-     *   node1[label = "{0: (0 0)|1: (1 1)|2: (2 2)}"];
+     * <p>
+     * node1[label = "{0: (0 0)|1: (1 1)|2: (2 2)}"];
      */
     @Override
     public String toDot() {
@@ -376,8 +430,24 @@ class LeafNode extends BPlusNode {
         // Note: LeafNode has two constructors. To implement fromBytes be sure to
         // use the constructor that reuses an existing page instead of fetching a
         // brand new one.
+        Page page = bufferManager.fetchPage(treeContext, pageNum);
+        Buffer buf = page.getBuffer();
+        byte nodeType = buf.get();
+        assert (nodeType == (byte) 1);
 
-        return null;
+        long sib = buf.getLong();
+        int count = buf.getInt();
+
+        ArrayList<DataBox> boxes = new ArrayList<>(count);
+        ArrayList<RecordId> records = new ArrayList<>(count);
+
+        for (int i = 0; i < count; i++) {
+            boxes.add(DataBox.fromBytes(buf, metadata.getKeySchema()));
+            records.add(RecordId.fromBytes(buf));
+        }
+
+        return new LeafNode(metadata, bufferManager, page, boxes, records,
+                sib == -1 ? Optional.empty() : Optional.of(sib), treeContext);
     }
 
     // Builtins ////////////////////////////////////////////////////////////////
@@ -391,9 +461,9 @@ class LeafNode extends BPlusNode {
         }
         LeafNode n = (LeafNode) o;
         return page.getPageNum() == n.page.getPageNum() &&
-               keys.equals(n.keys) &&
-               rids.equals(n.rids) &&
-               rightSibling.equals(n.rightSibling);
+                keys.equals(n.keys) &&
+                rids.equals(n.rids) &&
+                rightSibling.equals(n.rightSibling);
     }
 
     @Override
